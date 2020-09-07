c28ee95faa5ddcd5a74d841b9d03b4f8e5ec70d5
csimal <cedric.simal@student.unamur.be>
2020-09-07 18:44:55 +0200

    last commit

--- tsir-test.jl
+++ tsir-test.jl
@@ -2,6 +2,8 @@ using LightGraphs
 using Plots
 using DifferentialEquations
 using GraphPlot
+using GraphRecipes
+using ColorSchemes
 
 include("temporal-network.jl")
 include("temporal-sir.jl")
@@ -11,9 +13,9 @@ include("pair_approximation.jl")
 
 N = 100
 
-g = activity_driven(N, 100, 0.5, rand(N), 5, 5)
+g = activity_driven(N, 100, 0.5, rand(N), 3, 5)
 g = random_contacts(binary_tree(7), 100, 0.5, 0.5)
-g = random_contacts(watts_strogatz(N, 6, 0.1), 100, 0.5, 0.75)
+g = random_contacts(watts_strogatz(N, 6, 0.02), 100, 0.5, 0.75)
 g = random_contacts(erdos_renyi(N,0.05), 100, 0.5, 0.75)
 
 cl = ContactList(g)
@@ -21,10 +23,10 @@ ag = aggregate_network(g)
 
 N = nv(ag)
 
-gplot(ag)
+graphplot(ag, curves=false, method=:circular)
 
-β = 0.1
-γ = 0.05
+β = 0.8
+γ = 0.0
 
 x0 = ones(Int,N)
 x0[1] = 2
@@ -35,35 +37,51 @@ ts, output = event_based_tsir(cl, β, γ, x0)
 
 plot(ts, output[2], label="", linetype=:steppre)
 
-ts_av, out_av, err_av = average_tsir(cl, β, γ, x0, nsims=1000, tmax=100.0)
-ts_ib, out_ib = individual_based_continuous(g, β, γ, x0, tmax=100.0)
-ts_cb, out_cb = contact_based_continuous(g, β, γ, x0, tmax=100.0, saveat=ts_av)
+@time ts_av, out_av, err_av = average_tsir(cl, β, γ, x0, nsims=1000, tmax=50.0)
+@time ts_ib, out_ib = individual_based_continuous(g, β, γ, x0, tmax=50.0)
+@time ts_cb, out_cb = contact_based_continuous(g, β, γ, x0, tmax=50.0, saveat=ts_av)
 ts_pa, out_pa = pair_approximation(g, β, γ, x0, tmax=50.0, saveat=ts_av)
 
 cp_ag = ContactProcess(ag, SIR(β,γ))
-ts_av_ag, out_av_ag = average(cp_ag, x0, nsims=500, nbins=200, tmax=50.0)
-ts_ib_ag, out_ib_ag = meanfield(cp_ag, x0, tmax=50.0)
+@time ts_av_ag, out_av_ag = average(cp_ag, x0, nsims=1000, nbins=400, tmax=50.0)
+@time ts_ib_ag, out_ib_ag = meanfield(cp_ag, x0, tmax=50.0)
 @time ts_cb_ag, out_cb_ag = contact_based_continuous(cp_ag, x0, 50.0)
 @time ts_pa_ag, out_pa_ag = pair_approximation(cp_ag, x0, tmax=30.0)
+
+colours = ColorSchemes.tab10;
 
 plot(ts_av, (out_av[2,:])/N,
     linestyle=:dash,
     ylims=(0.0,1.05),
-    ylabel="I",
+    ylabel="Fraction of infected nodes",
     xlabel="Time",
     label="Average",
     legend=:topright,
-    fontfamily="Deja Vu"
+    fontfamily="Deja Vu",
+    color = colours[1]
+    )
+    plot!(ts_av, (out_av[2,:]+err_av[2,:])/N,
+    linestyle=:dot,
+    label="",
+    color= colours[1]
+    )
+    plot!(ts_av, (out_av[2,:]-err_av[2,:])/N,
+    linestyle=:dot,
+    label="",
+    color= colours[1]
     )
     plot!(ts_ib, sum(out_ib[2], dims=2)/N,
-    label="IB"
+    label="IB",
+    color = colours[2]
     )
     plot!(ts_cb, sum(out_cb[2], dims=2)/N,
-    label="CB"
+    label="CB",
+    color = colours[3]
     )
 
 plot!(ts_pa, sum(out_pa[2], dims=2)/N,
-    label="PA"
+    label="PA",
+    color = colours[4]
     )
 
 plot(ts_av, log10.(abs.(sum(out_cb[2]-out_pa[2], dims=2))),
@@ -78,18 +96,22 @@ plot(ts_av_ag, out_av_ag[2]/N,
     fontfamily="Deja Vu",
     label="Average (static)",
     ylims=(0.0,1.05),
-    ylabel="I",
-    xlabel="Time"
+    ylabel="Fraction of infected nodes",
+    xlabel="Time",
+    color = colours[1]
     )
     plot!(ts_ib_ag, sum(out_ib_ag[2], dims=2)/N,
-    label="IB (static)"
+    label="IB (static)",
+    color = colours[2]
     )
     plot!(ts_cb_ag, sum(out_cb_ag[2], dims=2)/N,
-    label="CB (static)"
+    label="CB (static)",
+    color = colours[3]
     )
 
 plot!(ts_pa_ag, sum(out_pa_ag[2], dims=2)/N,
-    label="PA (static)"
+    label="PA (static)",
+    color = colours[4]
     )
 
 
@@ -108,16 +130,18 @@ include("discrete-time-models.jl")
 
 N = 100
 g = activity_driven(N, 100, 1.0, rand(N), 5, 5)
-g = random_contacts(watts_strogatz(N, 6, 0.1), 100, 1.0, 0.25)
+g = random_contacts(watts_strogatz(N, 6, 0.02), 100, 1.0, 0.25)
 g = random_contacts(binary_tree(7), 100, 1.0, 0.5)
 g = random_contacts(path_graph(50), 100, 1.0, 0.25)
 g = random_contacts(star_graph(50), 100, 1.0, 0.75)
 g = random_contacts(erdos_renyi(N,0.05), 100, 1.0, 0.75)
+
 ag = aggregate_network(g)
 N = nv(ag)
 
+gplot(ag)
 
-β = 0.8
+β = 0.5
 γ = 0.1
 
 x0 = ones(Int,N)
@@ -127,11 +151,40 @@ x0 = fill(2,N)
 
 ts, output = event_based_dtsir(g, β, γ, x0)
 
-plot(ts, output[2],
-    label = "",
+plot(ts, output[1],
+    label = "S",
     fontfamily = "Deja Vu",
-    linetype = :steppre
-)
+    linetype = :steppost
+    )
+    plot!(ts, output[2],
+    label = "I",
+    linetype = :steppost)
+    plot!(ts, output[3],
+    label = "R",
+    linetype = :steppost)
+
+scatter(ts_av, out_av[1,:]/N, yerror=err_av[1,:]/N,
+    fontfamily = "Deja Vu",
+    ylims = (0.0,1.05),
+    ylabel = "Fraction of population",
+    xlabel = "Time",
+    label = "S",
+    #markersize=3,
+    color = colours[1],
+    lc=colours[1],
+    msc=colours[1],
+    #legend = :bottomright
+    )
+    scatter!(ts_av, out_av[2,:]/N, yerror=err_av[2,:]/N,
+    color = colours[2],
+    lc=colours[2],
+    msc=colours[2],
+    label = "I")
+    scatter!(ts_av, out_av[3,:]/N, yerror=err_av[3,:]/N,
+    color = colours[3],
+    lc=colours[3],
+    msc=colours[3],
+    label = "R")
 
 ts_av, out_av, err_av = average_dtsir(g, β, γ, x0, nsims=1000, tmax = 100.0, nbins=101)
 ts_ib, out_ib = individual_based_discrete(g, β, γ, x0, 100)
@@ -140,23 +193,27 @@ ts_cb, out_cb = contact_based_discrete(g, β, γ, x0, 100)
 scatter(ts_av, out_av[2,:]/N, yerror=err_av[2,:]/N,
     fontfamily = "Deja Vu",
     ylims = (0.0,1.05),
-    ylabel = "I",
+    ylabel = "Fraction of infected nodes",
     xlabel = "Time",
     label = "Average",
     markersize=3,
-    lc=:black,
-    msc=:black,
+    color = colours[1],
+    lc=colours[1],
+    msc=colours[1],
     legend = :bottomright
     )
     plot!(ts_ib, sum(out_ib[2], dims=2)/N,
     label="IB",
     marker=:circle,
-    markersize=3
+    markersize=3,
+    color = colours[2]
     )
-    plot!(ts_cb, sum(out_cb[2], dims=2)/N,
+
+plot!(ts_cb, sum(out_cb[2], dims=2)/N,
     label="CB",
     marker=:circle,
-    markersize=3
+    markersize=3,
+    color = colours[3]
     )
 
 
@@ -178,21 +235,24 @@ plot(ts_ag, out_ag[2],
 scatter(ts_av_ag, out_av_ag[2,:]/N, yerror=err_av_ag[2,:]/N,
     fontfamily = "Deja Vu",
     ylims = (0.0,1.05),
-    ylabel = "I",
+    ylabel = "Fraction of infected nodes",
     xlabel = "Time",
     label = "Average",
     markersize = 3,
-    lc=:black,
-    msc=:black,
+    color = colours[1],
+    lc= colours[1],
+    msc= colours[1],
     legend = :bottomright
     )
     plot!(ts_ib_ag, sum(out_ib_ag[2], dims=2)/N,
     label="IB",
     marker = :circle,
-    markersize = 3
+    markersize = 3,
+    color = colours[2]
     )
     plot!(ts_cb, sum(out_cb_ag[2], dims=2)/N,
     label="CB",
     marker = :circle,
-    markersize = 3
+    markersize = 3,
+    color = colours[3]
     )
